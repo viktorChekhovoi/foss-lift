8301d2d60e16a5fcbfc2052ae26df4a6772e91f9
Viktor Chekhovoi <viktor.chekhovoi@gmail.com>
2026-09-09 13:43:03 -0500

    Checkpoint for merge

---
+++ .codex/hooks/check-pending-messages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec '/home/viktor/.kettle/repo/bin/harness' --pending-message-hook '/tmp/kettle-worktrees/foss-lift/test_thread_2/implement-1/implementation/red/run.retry-2/workspace/.kettle/pending-system-messages'
